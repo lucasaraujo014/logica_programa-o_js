99040b7f4287345ab9ee6baca944bd964c3529f2
lucasaraujo014 <cleidetrindade17@gmail.com>
2024-03-14 15:33:40 -0300

    aritimetica

---
+++ operadores_aritimeticos.js
@@ -0,0 +1,10 @@
+const numero1 = 10
+const numero2 = 3.6
+
+console.log(`Os números que irão ser operados são ${numero1} e ${numero2}`);
+console.log(`A soma dos números é: ${numero1 + numero2} `);
+console.log(`A subtrção dos números é: ${numero1 - numero2} `);
+console.log(`A multiplicação dos números é: ${numero1 % numero2} `);
+console.log(`A divisão dos números é: ${numero1 / numero2} `);
+console.log(`O resto da divisão dos números é: ${numero1 % numero2} `);
+console.log(`A potência do número1 ao número2 é: ${numero1 ** numero2} `);
