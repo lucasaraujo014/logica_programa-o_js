1e3d6dfcc970d7dda827ce0fd478033ee1e9c734
lucasaraujo014 <cleidetrindade17@gmail.com>
2024-03-14 16:48:31 -0300

    aritimetica, atribuição e comparação

---
+++ operadores_atribuicao.js
@@ -0,0 +1,13 @@
+let numero = 10;
+
+console.log(`O numero ínicial é ${numero}`);
+console.log(`Somando 10: ${numero += 10}`);
+console.log(`Subraindo 10: ${numero -= 10}`);
+console.log(`Multiplicando por 10: ${numero *= 10}`);
+console.log(`Dvidido por 10: ${numero /= 10}`);
+console.log(`Obtendo o resto da divisão por 10: ${numero %= 10}`);
+console.log(`Elevando a potência de 10: ${numero **= 10}`);
+console.log(`Incrementado 1: ${++numero }`);
+console.log(`Decremento 1: ${--numero }`);
+console.log(`Numero final é: ${numero }`);
+
---
+++ operadores_comparacao.js
@@ -0,0 +1,8 @@
+const numero1 = 50
+const numero2 = "50"
+
+if (numero1 === numero2) {
+    console.log(`A condição é verdadeira`);
+}else{
+    console.log(`A condição é falsa`);
+}
